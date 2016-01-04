03587939464e4e99ec356340a18d55f09545274e
darksc <123189717@qq.com>
2016-01-04 10:45:36 +0800

    修改目录结构

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,18 @@ gulp.task('default', function() {
     // 将你的默认的任务代码放在这
 });
 
-gulp.task('script', function() {
+gulp.task('bowerScript', function() {
     // 找到
     gulp.src('bower_components/*/dist/*.min.js')
+        // 重新设置目录结构
+        .pipe(rename({dirname: ''}))
+        // 另存
+        .pipe(gulp.dest('public/javascripts'));
+});
+
+gulp.task('script', function() {
+    // 找到
+    gulp.src('static/*.js')
         // 重新设置目录结构
         .pipe(rename({dirname: ''}))
         // 另存
